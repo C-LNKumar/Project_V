d1ce47e0eae8cb526835ebb9044cf187c6545030
CHINNAM LAKSHMI NARASIMHA KUMAR <151651051+C-LNKumar@users.noreply.github.com>
2023-12-08 15:11:42 +0530

    Update Text to speech.py

--- Text to speech.py
+++ Text to speech.py
@@ -2,7 +2,7 @@ from gtts import gTTS
 
 language ="en"
 
-text = "love u Nikhil, hello i'm Kavya please accept mee"
+text = "This is the just a sample of the Text to Audio converting using the python Programming , And key Module to the Project V by LNK"
 
 speech = gTTS(text=text, lang= language,  tld="com.au")
 speech.save("example.mp3")
